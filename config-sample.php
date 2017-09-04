c9879b21f573c2dc481e702a178803ecd178f68c
Mike McDonald <mike90@gmail.com>
2017-09-04 10:16:51 -0400

    comments

--- config-sample.php
+++ config-sample.php
@@ -11,7 +11,12 @@ $dbname = "";
 $path = "http://google.com/";
 //Admin user
 $admin = "user";
+//Select a Time Zone from here http://php.net/manual/en/timezones.php
 date_default_timezone_set('America/New_York');
+//Formats the date. Change if you wish.
 $date = date('Y-m-d H:i:s');
+
+
+//Don't touch this.
 $user = $_SESSION['user'];
 ?>
